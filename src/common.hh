60fb6b9ef830f90cbc9b99876f4c7f27c6408e54
Markus Wolf <marco.pensallorto@gmail.com>
2012-01-23 08:53:31 +0100

    Missing file

---
+++ src/common.hh
@@ -0,0 +1,49 @@
+/*
+Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+ */
+
+/**
+ * @file common.hh
+ *
+ * @brief System wide definitions
+ *
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+#include <config.h>
+
+#include <utility>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+#include <tr1/unordered_set>
+#include <tr1/unordered_map>
+
+using std::pair;
+using std::string;
+using std::ostringstream;
+using std::ostream;
+using std::vector;
+
+using std::tr1::unordered_map;
+using std::tr1::unordered_set;
+
+#endif
