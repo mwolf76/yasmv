a3423cb7701537d9ada1ea5214c6e47c5c436913
Marco Pensallorto <marco.pensallorto@gmail.com>
2012-09-27 13:37:02 +0200

    ready for removal

--- src/sat/utils/solverlogger.cc
+++ src/sat/utils/solverlogger.cc
@@ -6,43 +6,4 @@
 
 namespace Minisat {
 
-  ostream &operator<<(ostream &out, const Lit &lit)
-  {
-    out << (sign(lit) ? "~" : "") << var(lit);
-
-    return out;
-  }
-
-  ostream &operator<<(ostream &out, const Clause *clause)
-  {
-    out << (clause->learnt() ? "L" : "") << "(";
-    for (int i = 0; i < clause->size()-1; ++i) {
-      out << (*clause)[i] << " | ";
-    }
-    out << (*clause)[clause->size()-1] << ")";
-
-    return out;
-  }
-
-  ostream &operator<<(ostream &out, const Clause &clause)
-  {
-    out << (&clause);
-
-    return out;
-  }
-
-  ostream &operator<<(ostream &out, const vec<Lit> &lits)
-  {
-    out << "{";
-
-    for (int i = 0; i < lits.size()-1; ++i) {
-      out << lits[i] << " ; ";
-    }
-
-    if (lits.size()) out << lits[lits.size()-1];
-    out << "}";
-
-    return out;
-  }
-
 }
