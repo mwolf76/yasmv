ea8ad960ea51d1dea0471363cef79bfc834ed3e9
Marco Pensallorto <marco.pensallorto@gmail.com>
2012-10-07 16:46:57 +0200

    dd tests kick off

---
+++ src/dd/tests/test_dd.cc
@@ -0,0 +1,17 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <expr.hh>
+#include <expr_mgr.hh>
+#include <expr_printer.hh>
+
+#include <type.hh>
+#include <type_mgr.hh>
+
+BOOST_AUTO_TEST_SUITE(tests)
+
+BOOST_AUTO_TEST_CASE(dd)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
