9195c023d437b7ea09c40b18116b685ffadb2740
Marco Pensallorto <marco.pensallorto@gmail.com>
2012-10-07 16:25:41 +0200

    one more for enums

--- src/type/tests/test_type.cc
+++ src/type/tests/test_type.cc
@@ -118,7 +118,7 @@ BOOST_AUTO_TEST_CASE(signed_type)
     BOOST_CHECK(   fit->is_signed() );
 }
 
-BOOST_AUTO_TEST_CASE(enum_type)
+BOOST_AUTO_TEST_CASE(enum_type_symbolic)
 {
     TypeMgr& tm = TypeMgr::INSTANCE();
     ExprMgr& em = ExprMgr::INSTANCE();
@@ -186,5 +186,75 @@ BOOST_AUTO_TEST_CASE(enum_type)
         BOOST_CHECK(in == tm.find_enum(dhl));
     }
 }
+
+BOOST_AUTO_TEST_CASE(enum_type_numeric)
+{
+    TypeMgr& tm = TypeMgr::INSTANCE();
+    ExprMgr& em = ExprMgr::INSTANCE();
+
+    Expr_ptr h = em.make_iconst(13);
+    Expr_ptr l = em.make_iconst(17);
+    Expr_ptr d = em.make_iconst(42);
+
+    ExprSet ev;
+    ev.insert(h);
+    ev.insert(l);
+    ev.insert(d);
+
+    Type_ptr in = tm.find_enum(ev);
+    BOOST_CHECK(! tm.is_boolean(in));
+
+    BOOST_CHECK(  tm.is_integer(in));
+    BOOST_CHECK(! tm.is_int_finite(in));
+    BOOST_CHECK(! tm.is_int_range(in));
+    BOOST_CHECK(  tm.is_int_enum(in));
+    BOOST_CHECK(  tm.is_enum(in));
+
+    BOOST_CHECK(! tm.is_instance(in));
+
+    // additional checks
+    EnumType_ptr et = dynamic_cast<EnumType_ptr>(in);
+    BOOST_REQUIRE( NULL != et );
+
+    BOOST_CHECK( 3 == et->literals().size() );
+
+    // try all possible different orderings (3! = 6)
+    { // #1
+        ExprSet lhd;
+        lhd.insert(l); lhd.insert(h); lhd.insert(d);
+        BOOST_CHECK(in == tm.find_enum(lhd));
+    }
+
+    { // #2
+        ExprSet ldh;
+        ldh.insert(l); ldh.insert(d); ldh.insert(h);
+        BOOST_CHECK(in == tm.find_enum(ldh));
+    }
+
+    { // #3
+        ExprSet hld;
+        hld.insert(h); hld.insert(l); hld.insert(d);
+        BOOST_CHECK(in == tm.find_enum(hld));
+    }
+
+    { // #4
+        ExprSet hdl;
+        hdl.insert(h); hdl.insert(d); hdl.insert(l);
+        BOOST_CHECK(in == tm.find_enum(hdl));
+    }
+
+    { // #5
+        ExprSet dlh;
+        dlh.insert(d); dlh.insert(l); dlh.insert(h);
+        BOOST_CHECK(in == tm.find_enum(dlh));
+    }
+
+    { // #6
+        ExprSet dhl;
+        dhl.insert(d); dhl.insert(h); dhl.insert(l);
+        BOOST_CHECK(in == tm.find_enum(dhl));
+    }
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
