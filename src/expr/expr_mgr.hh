9bfa15a8a2c41acdf162ea45f4f5b620ff6af877
Marco Pensallorto <marco.pensallorto@gmail.com>
2012-10-09 20:58:21 +0200

    Loads of work on algebraic compiler. Almost there.

--- src/expr/expr_mgr.hh
+++ src/expr/expr_mgr.hh
@@ -259,53 +259,56 @@ public:
             || (expr->f_symb == OCONST) ;
     }
 
-    inline bool is_unary_logical_bitwise(const Expr_ptr expr) const {
+    // expr inspectors, used by compiler as helpers to determine operands type
+    inline bool is_unary_logical(const Expr_ptr expr) const {
         assert(expr);
         ExprType symb = expr->f_symb;
 
         return (NOT == symb);
     }
 
-    inline bool is_binary_logical_bitwise(const Expr_ptr expr) const {
+    inline bool is_binary_logical(const Expr_ptr expr) const {
         assert(expr);
         ExprType symb = expr->f_symb;
 
-        return ( (AND == symb)
-                 || (OR  == symb)
-                 || (XOR == symb)
-                 || (XNOR == symb)
-                 || (IMPLIES == symb)
-                 || (IFF == symb));
+        return ((AND == symb)  ||
+                (OR  == symb)  ||
+                (XOR == symb)  ||
+                (XNOR == symb) ||
+                (IFF == symb)  ||
+                (IMPLIES == symb));
     }
 
-    inline bool is_unary_logical_only(const Expr_ptr expr) const {
-        assert(expr);
-        return false; /* there is no logical that is not also a bitwise */
-    }
-
-    inline bool is_binary_logical_only(const Expr_ptr expr) const {
-        assert(expr);
-        return false; /* there is no logical that is not also a bitwise */
-    }
-
-    inline bool is_unary_arithmetical_only(const Expr_ptr expr) const {
+    inline bool is_unary_arithmetical(const Expr_ptr expr) const {
         assert(expr);
         ExprType symb = expr->f_symb;
 
         return ( (NEG == symb) );
     }
 
-    inline bool is_binary_arithmetical_only(const Expr_ptr expr) const {
+    inline bool is_binary_arithmetical(const Expr_ptr expr) const {
         assert(expr);
         ExprType symb = expr->f_symb;
 
-        return ( (PLUS == symb)
-                 || (SUB == symb)
-                 || (DIV== symb)
-                 || (MUL== symb)
-                 || (MOD == symb)
-                 || (RSHIFT == symb)
-                 || (LSHIFT == symb));
+        return ((PLUS == symb)   ||
+                (SUB == symb)    ||
+                (DIV == symb)    ||
+                (MUL == symb)    ||
+                (MOD == symb)    ||
+                (RSHIFT == symb) ||
+                (LSHIFT == symb));
+    }
+
+    inline bool is_binary_relational(const Expr_ptr expr) const {
+        assert(expr);
+        ExprType symb = expr->f_symb;
+
+        return ((EQ == symb) ||
+                (NE == symb) ||
+                (LT == symb) ||
+                (LE == symb) ||
+                (GT == symb) ||
+                (GE == symb));
     }
 
     // singleton instance accessor
