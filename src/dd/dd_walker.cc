1e26dab770adbcab7e4e9c68f9a4aaf057cac11a
Marco Pensallorto <marco.pensallorto@gmail.com>
2012-11-15 03:22:11 +0100

    Added DD walkers

---
+++ src/dd/dd_walker.cc
@@ -0,0 +1,132 @@
+/**
+ * @file dd_walker.cc
+ * @brief DD leaves walker
+ *
+ * Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **/
+#include <common.hh>
+
+#include <dd_walker.hh>
+
+DDWalker::DDWalker(CuddMgr& owner)
+    : f_owner(owner)
+    , f_data(NULL)
+{ // DRIVEL << "Initialized walker @" << this << endl;
+}
+
+DDWalker::~DDWalker()
+{ // DRIVEL << "Deinitialized walker @" << this << endl;
+}
+
+DDWalker& DDWalker::operator() (ADD dd)
+{
+    // before walking hook
+    this->pre_hook();
+
+    dd_activation_record call(dd.getNode());
+
+    assert (NULL == f_data);
+    size_t nelems = f_owner.dd().getManager()->size;
+    f_data = (char *) malloc( nelems * sizeof(char));
+
+    /* 0 is false, 1 is true, 2 is don't care */
+    for (unsigned i = 0; i < nelems; ++ i) {
+        *( f_data + i ) = 2;
+    }
+
+    // setup toplevel act. record and perform walk
+    f_recursion_stack.push(call);
+    walk();
+
+    // after walking hook
+    this->post_hook();
+
+    /* release temp memory */
+    free(f_data); f_data = NULL;
+
+    return *this;
+}
+
+void DDWalker::walk ()
+{
+    size_t rec_level = f_recursion_stack.size();
+    assert (0 != rec_level);
+
+    size_t rec_goal = rec_level -1; // support re-entrant invocation
+    assert (0 == rec_goal);
+
+    DdNode *N, *Nv, *Nnv;
+
+    /* for fast access to the DD variables array */
+    char *cell;
+
+    while(f_recursion_stack.size() != rec_goal) {
+
+    call:
+        dd_activation_record curr = f_recursion_stack.top();
+
+        N = Cudd_Regular(curr.node);
+        cell = f_data + N->index;
+
+        if (curr.pc != DD_DEFAULT) {
+
+            /* common to all paths */
+            if (Cudd_IsComplement(curr.node)) {
+                Nv  = Cudd_Not(cuddT(N));
+                Nnv = Cudd_Not(cuddE(N));
+            }
+            else {
+                Nv  = cuddT(N);
+                Nnv = cuddE(N);
+            }
+
+            // restore caller location (simulate call return behavior)
+            switch(curr.pc) {
+            case DD_ELSE: goto entry_ELSE;
+            case DD_RETURN: goto entry_RETURN;
+            default: assert( false ); // unexpected
+            }
+        } /* pc != DEFAULT */
+
+        // DEFAULT:
+        assert(curr.node);
+
+        /* if node is a constand and fulfills condition, perform action on it */
+        if ( cuddIsConstant(N) && condition(curr.node) ) {
+            action(cuddV(curr.node));
+            continue; /* done with this node */
+        }
+
+        // entry_THEN: (fallthru)
+        *cell = 0; /* false */
+        f_recursion_stack.top().pc = DD_ELSE;
+        f_recursion_stack.push(dd_activation_record(Nnv));
+        goto call;
+
+    entry_ELSE:
+        *cell = 1; /* true */
+        f_recursion_stack.top().pc = DD_RETURN;
+        f_recursion_stack.push(dd_activation_record(Nv));
+        goto call;
+
+    entry_RETURN:
+        *cell = 2; /* don't care */
+
+        f_recursion_stack.pop();
+    } // while
+}
---
+++ src/dd/dd_walker.hh
@@ -0,0 +1,87 @@
+/**
+ *  @file dd_walker.hh
+ *  @brief DD algorithm-unaware walk pattern implementation
+ *
+ *  This module contains definitions and services that implement an
+ *  optimized storage for expressions. Expressions are stored in a
+ *  Directed Acyclic Graph (DAG) for data sharing.
+ *
+ *  Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **/
+
+#ifndef DD_WALKER_H
+#define DD_WALKER_H
+
+#include <common.hh>
+#include <cudd_mgr.hh>
+#include <cuddInt.h>
+
+// enums in C++ are non-extensible, thus we have to keep all possible
+// values together in one place. (for DDs things are simpler)
+typedef enum {
+    DD_DEFAULT,
+    DD_RETURN,
+    DD_ELSE
+} dd_entry_point;
+
+// reserved for walkers
+struct dd_activation_record {
+    dd_entry_point pc;
+    const DdNode *node;
+
+    dd_activation_record(const DdNode *n)
+        : pc(DD_DEFAULT)
+        , node(n)
+    {}
+};
+
+typedef stack<struct dd_activation_record> dd_walker_stack;
+
+class DDWalkerException : public Exception
+{
+public:
+    virtual const char* what() const throw() =0;
+};
+
+// walker base class
+class DDWalker {
+public:
+    DDWalker(CuddMgr& owner);
+    virtual ~DDWalker();
+
+    virtual DDWalker& operator() (ADD dd);
+
+protected:
+    dd_walker_stack f_recursion_stack;
+
+    // extra-hooks
+    virtual void pre_hook()
+    {}
+    virtual void post_hook()
+    {}
+
+    virtual void walk();
+
+    virtual bool condition(const DdNode *node) =0;
+    virtual void action(value_t value) =0;
+
+    CuddMgr& f_owner;
+    char *f_data; /* try to limit memory waste for caching */
+};
+
+#endif
