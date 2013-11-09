512ff52a0ec3b5dc4779ffafc815801a3fed54d2
Markus Wolf <marco.pensallorto@gmail.com>
2013-11-09 13:22:57 +0100

    Initial stab at the preprocessor

---
+++ src/model/preprocessor.cc
@@ -0,0 +1,485 @@
+/**
+ *  @file preprocessor.cc
+ *  @brief Expr preprocessor
+ *
+ *  Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  This module contains definitions and services that implement a
+ *  type inference engine. The type inference engine is implemented
+ *  using a simple walker pattern: (a) on preorder, return true if the
+ *  node has not yet been visited; (b) always do in-order (for binary
+ *  nodes); (c) perform proper type checking in post-order
+ *  hooks. Implicit conversion rules are designed to follow as closely
+ *  as possible section 6.3.1 of iso/iec 9899:1999 (aka C99)
+ *  standard. Type rules are implemented in the result_type methods of
+ *  the TypeMgr class.
+ *
+ **/
+
+#include <common.hh>
+
+#include <expr.hh>
+#include <preprocessor.hh>
+
+#include <model_mgr.hh>
+
+#include <proxy.hh>
+
+// uncommment following line to enable post_node_hook debug (verbose!)
+// #define DEBUG_PREPROCESSOR
+
+Preprocessor::Preprocessor(ModelMgr& owner)
+    : f_map()
+    , f_ctx_stack()
+    , f_expr_stack()
+    , f_env()
+    , f_owner(owner)
+{
+    DEBUG << "Created Preprocessor @" << this << endl;
+}
+
+Preprocessor::~Preprocessor()
+{ DEBUG << "Destroying Preprocessor @" << this << endl; }
+
+Type_ptr Preprocessor::process(Expr_ptr ctx, Expr_ptr expr)
+{
+    // remove previous results
+    f_ctx_stack.clear();
+    f_expr_stack.clear();
+
+    // clear the environment
+    f_env.clear();
+
+    // walk body in given ctx
+    f_ctx_stack.push_back(ctx);
+
+    // invoke walker on the body of the expr to be processed
+    (*this)(expr);
+
+    assert(1 == f_expr_stack.size());
+
+    POP_EXPR(res);
+    assert(NULL != res);
+
+    return res;
+}
+
+void Preprocessor::pre_hook()
+{}
+void Preprocessor::post_hook()
+{}
+
+void Preprocessor::pre_node_hook(Expr_ptr expr)
+{}
+
+void Preprocessor::post_node_hook(Expr_ptr expr)
+{}
+
+bool Preprocessor::walk_next_preorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_next_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(lhs);
+    PUSH_EXPR( f_em.make_next( lhs));
+}
+
+bool Preprocessor::walk_neg_preorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_neg_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_neg( lhs));
+}
+
+bool Preprocessor::walk_not_preorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_not_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_not( lhs));
+}
+
+bool Preprocessor::walk_add_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_add_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_add_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_add( lhs, rhs ));
+}
+
+bool Preprocessor::walk_sub_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_sub_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_sub_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_sub( lhs, rhs ));
+}
+
+bool Preprocessor::walk_div_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_div_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_div_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_div( lhs, rhs ));
+}
+
+bool Preprocessor::walk_mul_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_mul_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_mul_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_mul( lhs, rhs ));
+}
+
+bool Preprocessor::walk_mod_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_mod_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_mod_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_mod( lhs, rhs ));
+}
+
+bool Preprocessor::walk_and_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_and_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_and_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_and( lhs, rhs ));
+}
+
+bool Preprocessor::walk_or_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_or_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_or_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_or( lhs, rhs ));
+}
+
+bool Preprocessor::walk_xor_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_xor_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_xor_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_xor( lhs, rhs ));
+}
+
+bool Preprocessor::walk_xnor_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_xnor_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_xnor_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_xnor( lhs, rhs ));
+}
+
+bool Preprocessor::walk_implies_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_implies_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_implies_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_implies( lhs, rhs ));
+}
+
+bool Preprocessor::walk_iff_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_iff_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_iff_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_iff( lhs, rhs ));
+}
+
+bool Preprocessor::walk_lshift_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_lshift_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_lshift_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_lshift( lhs, rhs ));
+}
+
+bool Preprocessor::walk_rshift_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_rshift_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_rshift_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_rshift( lhs, rhs ));
+}
+
+bool Preprocessor::walk_eq_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_eq_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_eq_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_eq( lhs, rhs ));
+}
+
+bool Preprocessor::walk_ne_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_ne_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_ne_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_ne( lhs, rhs ));
+}
+
+bool Preprocessor::walk_gt_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_gt_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_gt_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_gt( lhs, rhs ));
+}
+
+bool Preprocessor::walk_ge_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_ge_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_ge_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_ge( lhs, rhs ));
+}
+
+bool Preprocessor::walk_lt_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_lt_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_lt_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_lt( lhs, rhs ));
+}
+
+bool Preprocessor::walk_le_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_le_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_le_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_le( lhs, rhs ));
+}
+
+bool Preprocessor::walk_ite_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_ite_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_ite_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    POP_EXPR(cond);
+    PUSH_EXPR(f_em.make_ite( cond, lhs, rhs ));
+}
+
+bool Preprocessor::walk_cond_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_cond_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_cond_postorder(const Expr_ptr expr)
+{ walk_ternary_cond_postorder(expr); }
+
+bool Preprocessor::walk_dot_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_dot_inorder(const Expr_ptr expr)
+{
+    assert( false ); // FIXME
+    return false;
+}
+void Preprocessor::walk_dot_postorder(const Expr_ptr expr)
+{
+    assert( false );
+}
+
+/* main entry-point */
+bool Preprocessor::walk_params_preorder(const Expr_ptr expr)
+{ substitute_expression( expr ); return false; }
+bool Preprocessor::walk_params_inorder(const Expr_ptr expr)
+{ assert( false ); return false; }
+void Preprocessor::walk_params_postorder(const Expr_ptr expr)
+{ assert(false); }
+
+bool Preprocessor::walk_subscript_preorder(const Expr_ptr expr)
+{ return true; }
+bool Preprocessor::walk_subscript_inorder(const Expr_ptr expr)
+{ return true; }
+void Preprocessor::walk_subscript_postorder(const Expr_ptr expr)
+{
+    POP_EXPR(rhs);
+    POP_EXPR(lhs);
+    PUSH_EXPR(f_em.make_subscript( lhs, rhs ));
+}
+
+bool Preprocessor::walk_comma_preorder(Expr_ptr expr)
+{ assert(false); return false; }
+bool Preprocessor::walk_comma_inorder(Expr_ptr expr)
+{ assert(false); return false; }
+void Preprocessor::walk_comma_postorder(Expr_ptr expr)
+{ assert(false); }
+
+void Preprocessor::walk_leaf(const Expr_ptr expr)
+{
+    ExprMgr& em = f_owner.em();
+
+    // is an integer const ..
+    if (em.is_numeric(expr)) {
+        PUSH_EXPR(expr);
+        return;
+    }
+
+    // .. or a symbol
+    if (em.is_identifier(expr)) {
+
+        /* traverse the env stack, subst with the first occurence, if any */
+        ExprPairStack::reverse_iterator env_iter;
+        for (env_iter = f_env.rbegin(); env_iter != f_env.rend(); ++ env_iter) {
+            ExprPairStack entry = (*env_iter);
+
+            if (entry.first() == expr) {
+                expr = entry.second();
+                break;
+            }
+        }
+
+        /* Symb resolution */
+        ResolverProxy proxy;
+        ISymbol_ptr symb = proxy.symbol(f_ctx_stack.back(), expr);
+
+        if (symb->is_const()) {
+            Expr_ptr res = symb->as_const().expr();
+            PUSH_EXPR(res);
+            return;
+        }
+        else if (symb->is_literal()) {
+            Expr_ptr res = symb->as_literal().expr();
+            PUSH_EXPR(res);
+            return;
+        }
+        else if (symb->is_enum()) { // meta type
+            Expr_ptr res = symb->as_enum().type();
+            PUSH_EXPR(res);
+            return;
+        }
+        else if (symb->is_array()) { // meta type
+            Type_ptr res = symb->as_array().type();
+            PUSH_TYPE(res);
+            return;
+        }
+        else if (symb->is_variable()) {
+            Expr_ptr res = symb->as_variable().type();
+            PUSH_EXPR(res);
+            return;
+        }
+        else if (symb->is_define()) {
+            assert( false );
+        }
+    }
+
+    assert(false); // unexpected
+}
+
+ExprVector Preprocessor::traverse_param_list(ExprVector& params, const Expr_ptr expr)
+{
+    if (f_em.is_comma( expr)) {
+        traverse_param_list( params, expr->lhs());
+        traverse_param_list( params, expr->rhs());
+    }
+    params.push_back( expr);
+}
+
+void Preprocessor::substitute_expression(const Expr_ptr expr)
+{
+    ResolverProxy proxy;
+
+    /* LHS -> define name, extract formals for definition */
+    assert ( f_em.is_identifier( expr->lhs()));
+    IDefine_ptr define = proxy.symbol(f_ctx_stack.back(), name) -> as_define();
+    ExprVector& formals = define -> formals();
+
+    /* RHS -> comma separated lists of actual parameters */
+    ExprVector actuals; traverse_param_list( actuals, expr -> rhs());
+
+    /* Populate the subst environment */
+    assert( formals.size() == actuals.size());
+    for (ExprVector::iterator ai = actuals.begin(),
+             ExprVector::iterator fi = formals.begin();
+         ai != actuals.end(); ++ ai, ++ fi) {
+
+        Expr_ptr actual = (*ai);
+        Expr_ptr formal = (*fi);
+        f_env.push_back( make_pair <Expr_ptr, Expr_ptr>
+                         ( formal, actual ));
+    }
+
+    /* Here comes a bit of magic: we just relaunch the preprocessor on the
+       define body, to perform the substitution :-D */
+    (*this)(symb->define.body());
+
+    /* Restore previous environment */
+    for (ExprVector::iterator ai = actuals.begin();
+         ai != actuals.end(); ++ ai ) {
+        env.pop_back();
+    }
+}
---
+++ src/model/preprocessor.hh
@@ -0,0 +1,169 @@
+/**
+ *  @file preprocessor.hh
+ *  @brief Expr preprocessor used in Define w/ param substitution
+ *
+ *  Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **/
+
+#ifndef PREPROCESSOR_H
+#define PREPROCESSOR_H
+
+#include <model.hh>
+#include <expr_walker.hh>
+
+// NOTE: here we're using a vector in order to bypass STL stack
+// interface limitations. (i.e. absence of clear())
+typedef vector< pair< Expr_ptr, Expr_ptr>> ExprPairStack;
+typedef vector<Expr_ptr> ExprStack;
+typedef vector<IDefine_ptr> DefinesStack;
+
+/* shortcuts to simplify manipulation of the internal expr stack */
+#define POP_EXPR(op)                              \
+    const Expr_ptr op = f_expr_stack.back();      \
+    f_expr_stack.pop_back()
+
+#define PUSH_EXPR(tp)                             \
+    f_expr_stack.push_back(tp)
+
+/* shortcuts to simplify manipulation of the internal define stack */
+#define POP_DEFINE(op)                              \
+    const Define_ptr op = f_define_stack.back();    \
+    f_define_stack.pop_back()
+
+#define PUSH_DEFINE(tp)                             \
+    f_define_stack.push_back(tp)
+
+class ModelMgr;
+class Preprocessor : public ExprWalker {
+public:
+    Preprocessor(ModelMgr& owner);
+    ~Preprocessor();
+
+    // walker toplevel
+    Expr_ptr process(Expr_ptr ctx, Expr_ptr expr);
+
+protected:
+    void pre_hook();
+    void post_hook();
+
+    void pre_node_hook(Expr_ptr expr);
+    void post_node_hook(Expr_ptr expr);
+
+    // walker interface
+    bool walk_next_preorder(const Expr_ptr expr);
+    void walk_next_postorder(const Expr_ptr expr);
+    bool walk_neg_preorder(const Expr_ptr expr);
+    void walk_neg_postorder(const Expr_ptr expr);
+    bool walk_not_preorder(const Expr_ptr expr);
+    void walk_not_postorder(const Expr_ptr expr);
+    bool walk_add_preorder(const Expr_ptr expr);
+    bool walk_add_inorder(const Expr_ptr expr);
+    void walk_add_postorder(const Expr_ptr expr);
+    bool walk_sub_preorder(const Expr_ptr expr);
+    bool walk_sub_inorder(const Expr_ptr expr);
+    void walk_sub_postorder(const Expr_ptr expr);
+    bool walk_div_preorder(const Expr_ptr expr);
+    bool walk_div_inorder(const Expr_ptr expr);
+    void walk_div_postorder(const Expr_ptr expr);
+    bool walk_mul_preorder(const Expr_ptr expr);
+    bool walk_mul_inorder(const Expr_ptr expr);
+    void walk_mul_postorder(const Expr_ptr expr);
+    bool walk_mod_preorder(const Expr_ptr expr);
+    bool walk_mod_inorder(const Expr_ptr expr);
+    void walk_mod_postorder(const Expr_ptr expr);
+    bool walk_and_preorder(const Expr_ptr expr);
+    bool walk_and_inorder(const Expr_ptr expr);
+    void walk_and_postorder(const Expr_ptr expr);
+    bool walk_or_preorder(const Expr_ptr expr);
+    bool walk_or_inorder(const Expr_ptr expr);
+    void walk_or_postorder(const Expr_ptr expr);
+    bool walk_xor_preorder(const Expr_ptr expr);
+    bool walk_xor_inorder(const Expr_ptr expr);
+    void walk_xor_postorder(const Expr_ptr expr);
+    bool walk_xnor_preorder(const Expr_ptr expr);
+    bool walk_xnor_inorder(const Expr_ptr expr);
+    void walk_xnor_postorder(const Expr_ptr expr);
+    bool walk_implies_preorder(const Expr_ptr expr);
+    bool walk_implies_inorder(const Expr_ptr expr);
+    void walk_implies_postorder(const Expr_ptr expr);
+    bool walk_iff_preorder(const Expr_ptr expr);
+    bool walk_iff_inorder(const Expr_ptr expr);
+    void walk_iff_postorder(const Expr_ptr expr);
+    bool walk_lshift_preorder(const Expr_ptr expr);
+    bool walk_lshift_inorder(const Expr_ptr expr);
+    void walk_lshift_postorder(const Expr_ptr expr);
+    bool walk_rshift_preorder(const Expr_ptr expr);
+    bool walk_rshift_inorder(const Expr_ptr expr);
+    void walk_rshift_postorder(const Expr_ptr expr);
+    bool walk_eq_preorder(const Expr_ptr expr);
+    bool walk_eq_inorder(const Expr_ptr expr);
+    void walk_eq_postorder(const Expr_ptr expr);
+    bool walk_ne_preorder(const Expr_ptr expr);
+    bool walk_ne_inorder(const Expr_ptr expr);
+    void walk_ne_postorder(const Expr_ptr expr);
+    bool walk_gt_preorder(const Expr_ptr expr);
+    bool walk_gt_inorder(const Expr_ptr expr);
+    void walk_gt_postorder(const Expr_ptr expr);
+    bool walk_ge_preorder(const Expr_ptr expr);
+    bool walk_ge_inorder(const Expr_ptr expr);
+    void walk_ge_postorder(const Expr_ptr expr);
+    bool walk_lt_preorder(const Expr_ptr expr);
+    bool walk_lt_inorder(const Expr_ptr expr);
+    void walk_lt_postorder(const Expr_ptr expr);
+    bool walk_le_preorder(const Expr_ptr expr);
+    bool walk_le_inorder(const Expr_ptr expr);
+    void walk_le_postorder(const Expr_ptr expr);
+    bool walk_ite_preorder(const Expr_ptr expr);
+    bool walk_ite_inorder(const Expr_ptr expr);
+    void walk_ite_postorder(const Expr_ptr expr);
+    bool walk_cond_preorder(const Expr_ptr expr);
+    bool walk_cond_inorder(const Expr_ptr expr);
+    void walk_cond_postorder(const Expr_ptr expr);
+    bool walk_dot_preorder(const Expr_ptr expr);
+    bool walk_dot_inorder(const Expr_ptr expr);
+    void walk_dot_postorder(const Expr_ptr expr);
+    bool walk_params_preorder(const Expr_ptr expr);
+    bool walk_params_inorder(const Expr_ptr expr);
+    void walk_params_postorder(const Expr_ptr expr);
+    bool walk_subscript_preorder(const Expr_ptr expr);
+    bool walk_subscript_inorder(const Expr_ptr expr);
+    void walk_subscript_postorder(const Expr_ptr expr);
+    bool walk_comma_preorder(const Expr_ptr expr);
+    bool walk_comma_inorder(const Expr_ptr expr);
+    void walk_comma_postorder(const Expr_ptr expr);
+
+    void walk_leaf(const Expr_ptr expr);
+
+private:
+    // probably not really necessary, we keep it for now
+    ExprStack f_ctx_stack;
+
+    // Results stack
+    ExprStack f_expr_stack;
+
+    // Not terribly efficient but easier to implement nested envs with
+    ExprPairStack f_env;
+
+    // managers
+    ModelMgr& f_owner;
+
+    void substitute_expression(const Expr_ptr expr);
+    ExprVector traverse_param_list(const Expr_ptr expr);
+};
+
+#endif
