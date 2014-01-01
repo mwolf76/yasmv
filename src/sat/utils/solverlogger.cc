709479ef9bb0d276b75b774740594a8b24fa78b1
Markus Wolf <marco.pensallorto@gmail.com>
2014-01-01 13:02:38 +0100

    Moved Minisat solver in a separate sub-folder. Adjusted build accordingly

--- src/sat/utils/solverlogger.cc
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-// solverlogger.cc: Output operators for various solver types (Lit, Clause, ...)
-// Author: Alberto Griggio
-
-#include "utils/solverlogger.hh"
-
-namespace Minisat {
-
-}
