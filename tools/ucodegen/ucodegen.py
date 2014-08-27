449ad7f63eef8c2188d44f8a2cc3e310c0220a8e
Marco Pensallorto <mpensallorto@imolinfo.it>
2014-08-27 07:58:29 +0200

    Introducing more operators w/ signedness

--- tools/ucodegen/ucodegen.py
+++ tools/ucodegen/ucodegen.py
@@ -8,71 +8,11 @@ This tool is part of the YASMINE project.
 
 import re
 import sys
+import datetime
 import os.path
 import subprocess
 
 workdir = "/home/markus/Code/github/gnuSMV/tools/ucodegen/workdir"
-
-stanzaTmpl = """
-{
-  clause c;
-  c.push_back( 10 );
-  c.push_back( 30 );
-  c.push_back( 12 );
-  f_ucode.push_back( c );
-}
-"""
-def stanza( clause ):
-    res = "{\n         clause c;\n"
-
-    for c in clause:
-        res += "         c.push_back( %d );\n" % c
-
-    res += "         f_ucode.push_back( c );\n      }\n"
-    return res
-
-moduleTmpl = """/* autogenerate source - do not edit */
-#include <vector>
-using std::vector;
-
-#include <iostream>
-using std::cout;
-using std::endl;
-
-typedef vector<int> clause;
-typedef vector<clause> microcode;
-
-class %(op)s%(width)d {
-
-public:
-    %(op)s%(width)d() {
-      %(contents)s
-    }
-
-    const microcode& ucode() const {
-        return f_ucode;
-    }
-
-private:
-    microcode f_ucode;
-
-};
-
-int main() {
-    %(op)s%(width)d() test;
-
-    const microcode& ucode = test.ucode();
-    for (microcode::const_iterator i = ucode.begin(); i != ucode.end(); ++ i) {
-
-        const clause& cl = (*i);
-        for (clause::const_iterator j = cl.begin(); j != cl.end(); ++ j) {
-            cout << *j << " ";
-        }
-        cout << endl;
-
-    }
-}
-"""
 
 # parser states
 (
@@ -83,135 +23,147 @@ int main() {
     ST_WRITE_OUT,
 ) = range(5)
 
-def main():
+def gen_binary_ops_microcode():
 
     # list of operators to be processed
-    ops = [ ( 'mul', '*' ) ]
+    ops = [
+        ( 'add', '+' ),
+        ( 'sub', '-' ),
+        ( 'mul', '*' ),
+        ( 'div', '/' ),
+        ( 'mod', 'mod' ),
+    ]
 
-    width = 8 # testing
     for (opName, opSymb) in ops:
 
-        print opName, opSymb, width
+        for signedness in ('unsigned', 'signed'):
 
-        # 1. create SMV model with given op
-        modelName = os.path.join( workdir, opName ) + "%d.smv" % (width, )
+            for width in range(1, 65):
 
-        model = file (modelName, "wt")
-        model.write('MODULE main\n\n')
-        model.write('VAR x : unsigned word[%d];\n'  % width)
-        model.write('VAR y : unsigned word[%d];\n'  % width)
-        model.write('VAR z : unsigned word[%d];\n\n'  % width)
+                sourceName = os.path.join( workdir, signedness[0] + opName ) + "%d.json" % width
+                source = file (sourceName, "wt" )
 
-        model.write('INIT z = x %s y\n'  % opSymb)
-        model.close()
+                print "Generating microcode for %s (%s), width = %d" % (opName, opSymb, width)
 
-        # 2. create a CMD script to generate CNF using custom version of NuSMV2
-        scriptName = os.path.join( workdir, opName ) + ".cmd"
+                # 1. create SMV model with given op
+                modelName = os.path.join( workdir, signedness[0] + opName ) + "%d.smv" % (width, )
 
-        cnfName = os.path.join( workdir, opName ) + "%d" % (width, )
+                model = file (modelName, "wt")
+                model.write('MODULE main\n\n')
+                model.write('VAR x : %s word[%d];\n'  % (signedness, width))
+                model.write('VAR y : %s word[%d];\n'  % (signedness, width))
+                model.write('VAR z : %s word[%d];\n\n' % (signedness, width))
 
-        script = file (scriptName, "wt")
-        script.write ('go_bmc\n')
-        script.write ('gen_invar_bmc -p TRUE -o %s\n' % cnfName )
-        script.write ('quit\n')
-        script.close ()
+                model.write('INIT z = x %s y\n'  % opSymb)
+                model.close()
 
-        # 3. invoke custom NuSMV2 to generate CNF
-        subprocess.call( [ 'NuSMV', '-quiet', '-source', scriptName, modelName ] )
+                # 2. create a CMD script to generate CNF using custom version of NuSMV2
+                scriptName = os.path.join( workdir, "build_microcode" ) + ".cmd"
 
-        # 4. process generated .dimacs file
-        dimacsName = os.path.join( workdir, opName ) + "%d.dimacs" % (width, )
+                cnfName = os.path.join( workdir, signedness[0] + opName ) + "%d" % (width, )
 
-        ac, x, y, z, seen, ucode = None, [], [], [], [], []
-        parserState = ST_WAIT_TIME0
+                script = file (scriptName, "wt")
+                script.write ('go_bmc\n')
+                script.write ('gen_invar_bmc -p TRUE -o %s\n' % cnfName )
+                script.write ('quit\n')
+                script.close ()
 
-        dimacs = file(dimacsName)
-        for line in dimacs:
+                # 3. invoke custom NuSMV2 to generate CNF
+                subprocess.call( [ 'NuSMV', '-quiet', '-source', scriptName, modelName ] )
 
-            if parserState == ST_WAIT_TIME0:
+                # 4. process generated .dimacs file
+                dimacsName = os.path.join( workdir, opName ) + "%d.dimacs" % (width, )
 
-                if re.match( "c @@@@@ Time 0", line):
-                    parserState = ST_FETCH_VARS
-                    continue
+                ac, x, y, z, seen, ucode = None, [], [], [], [], []
+                parserState = ST_WAIT_TIME0
 
-            elif parserState == ST_FETCH_VARS:
-                match = re.match( 'c CNF variable (\d+) => Time 0, Model Variable (\S)\.(\d+)',
-                                  line )
-                if (match):
-                    (index, var, bit) = match.groups()
-                    index = int(index)
-                    bit = int(bit)
+                dimacs = file(dimacsName)
+                for line in dimacs:
 
-                    assert var in ( 'x', 'y', 'z')
-                    assert bit in range( width)
+                    if parserState == ST_WAIT_TIME0:
 
-                    if var == 'x':
-                        x.insert( 0, index)
-                    elif var == 'y':
-                        y.insert( 0, index)
-                    elif var == 'z':
-                        z.insert( 0, index)
+                        if re.match( "c @@@@@ Time 0", line):
+                            parserState = ST_FETCH_VARS
+                            continue
 
-                elif re.match( "c @@@@@ Time 1", line ):
-                    parserState = ST_WAIT_PROBLEM
-                    continue
+                    elif parserState == ST_FETCH_VARS:
+                        match = re.match( 'c CNF variable (\d+) => Time 0, Model Variable (\S)\.(\d+)',
+                                          line )
+                        if (match):
+                            (index, var, bit) = match.groups()
+                            index = int(index)
+                            bit = int(bit)
 
-            elif parserState == ST_WAIT_PROBLEM:
+                            assert var in ( 'x', 'y', 'z')
+                            assert bit in range( width)
 
-                if re.match( "p cnf", line):
-                    parserState = ST_READ_PROBLEM
-                    continue
+                            if var == 'x':
+                                x.insert( 0, index)
+                            elif var == 'y':
+                                y.insert( 0, index)
+                            elif var == 'z':
+                                z.insert( 0, index)
 
-            elif parserState == ST_READ_PROBLEM:
+                        elif re.match( "c @@@@@ Time 1", line ):
+                            parserState = ST_WAIT_PROBLEM
+                            continue
 
-                if re.match( "c End of dimacs dumping", line):
-                    parserState = ST_WRITE_OUT
-                    continue
+                    elif parserState == ST_WAIT_PROBLEM:
 
-                numbers = map( int, line.split(" ")[:-1])
-                assert (ac is not None or 1 == len(numbers))
+                        if re.match( "p cnf", line):
+                            parserState = ST_READ_PROBLEM
+                            continue
 
-                if ac is None:
-                    assert 1 == len(numbers)
-                    ac = numbers[0]
+                    elif parserState == ST_READ_PROBLEM:
 
-                rewrite = []
-                for n in numbers:
+                        if re.match( "c End of dimacs dumping", line):
+                            parserState = ST_WRITE_OUT
+                            continue
 
-                    if n == ac:
-                        rewrite.append(0)
-                        continue
-                    elif n in z:
-                        rewrite.append( 0 * width + z.index(n))
-                        continue
-                    elif n in x:
-                        rewrite.append( 1 * width + x.index(n))
-                        continue
-                    elif n in y:
-                        rewrite.append( 2 * width + y.index(n))
-                        continue
-                    elif n not in seen:
-                        seen.append(n)
+                        numbers = map( int, line.split(" ")[:-1])
+                        assert (ac is not None or 1 == len(numbers))
 
-                    rewrite.append( 3 * width + seen.index(n))
+                        if ac is None:
+                            assert 1 == len(numbers)
+                            ac = numbers[0]
 
-                ucode.append(rewrite)
+                        rewrite = []
+                        for n in numbers:
 
-        assert parserState == ST_WRITE_OUT
-        assert( len(x) == len(y) and len(x) == len(z))
+                            if n == ac:
+                                rewrite.append(0)
+                                continue
+                            elif n in z:
+                                rewrite.append( 0 * width + z.index(n))
+                                continue
+                            elif n in x:
+                                rewrite.append( 1 * width + x.index(n))
+                                continue
+                            elif n in y:
+                                rewrite.append( 2 * width + y.index(n))
+                                continue
+                            elif n not in seen:
+                                seen.append(n)
 
-        print "x = %s" % x
-        print "y = %s" % y
-        print "z = %s" % z
+                            rewrite.append( 3 * width + seen.index(n))
 
-        sourceName = os.path.join( workdir, opName ) + "%d.cc" % (width, )
-        source = file (sourceName, "wt" )
+                        ucode.append(rewrite)
 
-        source.write ( moduleTmpl % {
-            'op': opName,
-            'width': width,
-            'contents': "\n      ".join( map( stanza, ucode ))
-        })
+                assert parserState == ST_WRITE_OUT
+                assert( len(x) == len(y) and len(x) == len(z))
+
+                source.write( "{ \"operator\": \"%(op)s\",\n   \"width\": \"%(width)d\",\n  \"cnf\": [ %(data)s ]}" % {
+                    'op': signedness[0] + opName,
+                    'width': width,
+                    'cnf': ", ".join( map( str, ucode[1:] ))
+                })
+
+def clean():
+    subprocess.call( [ 'rm', '-f', 'workdir/*' ] )
+
+def main():
+    clean()
+    gen_binary_ops_microcode()
 
 if __name__ == "__main__":
     main()
