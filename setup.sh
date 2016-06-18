472f5ecde01f97294972bbb898c75f2dfddc516b
Markus Wolf <marco.pensallorto@gmail.com>
2016-06-18 12:33:03 +0200

    updated READMEs and build

--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-./configure CFLAGS="-O0 -g -D __STDC_LIMIT_MACROS -D __STDC_FORMAT_MACROS -DPIC -fPIC" CXXFLAGS="-O0 -g -D __STDC_LIMIT_MACROS -D __STDC_FORMAT_MACROS -DPIC -fPIC -Wall -Werror"
+
+# a few useful standard defines
+DEFINES="-D __STDC_LIMIT_MACROS -D __STDC_FORMAT_MACROS -DPIC"
+
+# compilation options for debugging, all + extra warnings enabled. Any warning is fatal.
+OPTIONS="-g -O0 -fPIC -Wall -Werror"
+
+ALLFLAGS="$DEFINES $OPTIONS"
+
+# invoke configure script with above settings
+./configure CFLAGS="-O2" CXXFLAGS="$ALLFLAGS"
