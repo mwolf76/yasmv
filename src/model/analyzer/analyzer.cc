f99d3ef349b3c15ff4992ccbdd1bef8f8894e862
Marco Pensallorto <marco.pensallorto@gmail.com>
2016-11-16 16:29:07 +0100

    Missing files

---
+++ src/model/analyzer/analyzer.cc
@@ -0,0 +1,366 @@
+/**
+ * @file analyzer.cc
+ * @brief Semantic analyzer module
+ *
+ * Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ **/
+
+#include <common.hh>
+
+#include <expr/expr.hh>
+#include <type/type.hh>
+#include <symb/proxy.hh>
+
+#include <model/analyzer/analyzer.hh>
+
+Analyzer::Analyzer(ModelMgr& owner)
+    : f_ctx_stack()
+    , f_owner(owner)
+{
+    const void *instance
+        (this);
+
+    DRIVEL
+        << "Created Analyzer @"
+        << instance
+        << std::endl;
+}
+
+Analyzer::~Analyzer()
+{
+    const void *instance
+        (this);
+
+    DRIVEL
+        << "Destroying Analyzer @"
+        << instance
+        << std::endl;
+}
+
+void Analyzer::process(Expr_ptr expr, Expr_ptr ctx)
+{
+    // remove previous results
+    f_expr_stack.clear();
+    f_ctx_stack.clear();
+
+    // walk body in given ctx
+    f_ctx_stack.push_back(ctx);
+
+    // invoke walker on the body of the expr to be processed
+    (*this)(expr);
+
+    assert(! f_expr_stack.size());
+}
+
+bool Analyzer::walk_F_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_F_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_G_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_G_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_X_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_X_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_U_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_U_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_U_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_R_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_R_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_R_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_next_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_next_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_neg_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_neg_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_not_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_not_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_bw_not_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_bw_not_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_add_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_add_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_add_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_sub_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_sub_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_sub_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_div_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_div_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_div_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_mul_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_mul_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_mul_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_mod_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_mod_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_mod_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_and_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_and_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_and_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_bw_and_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_bw_and_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_bw_and_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_or_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_or_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_or_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_bw_or_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_bw_or_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_bw_or_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_bw_xor_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_bw_xor_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_bw_xor_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_bw_xnor_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_bw_xnor_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_bw_xnor_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_implies_preorder(const Expr_ptr expr)
+{
+    /* checks for `top-levelness` in assignments ;-) */
+    ExprMgr& em
+        (this->owner().em());
+
+    if (1 != f_expr_stack.size() &&
+        em.is_eq(expr->rhs()) &&
+        em.is_next(expr->rhs()->lhs()) &&
+        em.is_identifier(expr->rhs()->lhs()->lhs()))
+        throw BadAssignmentContext(expr);
+
+    return true;
+}
+
+bool Analyzer::walk_implies_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_implies_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_cast_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_cast_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_cast_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_type_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_type_inorder(const Expr_ptr expr)
+{ assert(false); /* unreachable */ return false; }
+void Analyzer::walk_type_postorder(const Expr_ptr expr)
+{ assert(false); /* unreachable */ }
+
+bool Analyzer::walk_lshift_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_lshift_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_lshift_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_rshift_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_rshift_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_rshift_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_eq_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_eq_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_eq_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_ne_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_ne_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_ne_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_gt_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_gt_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_gt_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_ge_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_ge_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_ge_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_lt_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_lt_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_lt_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_le_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_le_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_le_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_ite_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_ite_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_ite_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_cond_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_cond_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_cond_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_dot_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_dot_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_dot_postorder(const Expr_ptr expr)
+{}
+
+/* on-demand preprocessing to expand defines delegated to Preprocessor */
+bool Analyzer::walk_params_preorder(const Expr_ptr expr)
+{
+    Expr_ptr ctx
+        (f_ctx_stack.back());
+
+    (*this)(f_owner.preprocess(expr, ctx));
+
+    return false;
+}
+bool Analyzer::walk_params_inorder(const Expr_ptr expr)
+{ assert( false ); return false; /* unreachable */ }
+void Analyzer::walk_params_postorder(const Expr_ptr expr)
+{ assert( false ); return ; /* unreachable */ }
+
+bool Analyzer::walk_params_comma_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_params_comma_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_params_comma_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_subscript_preorder(const Expr_ptr expr)
+{ return true; }
+bool Analyzer::walk_subscript_inorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_subscript_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_array_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_array_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_array_comma_preorder(Expr_ptr expr)
+{ return true; }
+
+bool Analyzer::walk_array_comma_inorder(Expr_ptr expr)
+{ return true; }
+
+void Analyzer::walk_array_comma_postorder(Expr_ptr expr)
+{}
+
+bool Analyzer::walk_set_preorder(const Expr_ptr expr)
+{ return true; }
+void Analyzer::walk_set_postorder(const Expr_ptr expr)
+{}
+
+bool Analyzer::walk_set_comma_preorder(Expr_ptr expr)
+{ return true; }
+
+bool Analyzer::walk_set_comma_inorder(Expr_ptr expr)
+{ return true; }
+
+void Analyzer::walk_set_comma_postorder(Expr_ptr expr)
+{}
+
+void Analyzer::walk_leaf(const Expr_ptr expr)
+{}
---
+++ src/model/analyzer/analyzer.hh
@@ -0,0 +1,65 @@
+/**
+ * @file analyzer.hh
+ * @brief Semantic analyzer
+ *
+ * This header file contains the declarations required to implement
+ * the type checking of temporal logic expressions.
+ *
+ * Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ **/
+
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+#include <expr/walker/walker.hh>
+
+#include <type/type.hh>
+#include <type/type_mgr.hh>
+
+class ModelMgr;
+class Analyzer : public ExprWalker {
+public:
+    Analyzer(ModelMgr& owner);
+    ~Analyzer();
+
+    // walker toplevel
+    void process(Expr_ptr expr, Expr_ptr ctx);
+
+    inline ModelMgr& owner()
+    { return f_owner; }
+
+protected:
+    void pre_hook();
+    void post_hook();
+
+    void pre_node_hook(Expr_ptr expr);
+    void post_node_hook(Expr_ptr expr);
+
+    LTL_HOOKS; OP_HOOKS;
+    void walk_leaf(const Expr_ptr expr);
+
+private:
+    ExprVector f_expr_stack;
+    ExprVector f_ctx_stack;
+
+    // managers
+    ModelMgr& f_owner;
+};
+
+#endif /* ANALYZER_H */
