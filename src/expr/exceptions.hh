054a89f5df81d78ee1d68480dce303e1edb73316
Marco Pensallorto <marco.pensallorto@gmail.com>
2016-12-06 12:23:00 +0100

    missing file

---
+++ src/expr/exceptions.hh
@@ -0,0 +1,37 @@
+/**
+ * @file expr/exceptions.hh
+ * @brief Expr system module header file, exception classes.
+ *
+ * Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ **/
+
+#ifndef EXPR_EXCEPTIONS_H
+#define EXPR_EXCEPTIONS_H
+
+/** Exception classes */
+class ExprException : public Exception {
+public:
+    ExprException(const std::string& subexpr,
+                  const std::string& message="")
+        : Exception("ExprException", subexpr, message)
+    {}
+};
+
+#endif /* EXPR_EXCEPTIONS_H */
+
