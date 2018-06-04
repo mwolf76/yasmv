9fb2ad418c9dc18dcc92c9cf0f72f08d3722b47f
Marco Pensallorto <marco.pensallorto@gmail.com>
2018-06-04 23:13:15 +0200

    Fixed functional tests

--- tools/run-functional-tests.sh
+++ tools/run-functional-tests.sh
@@ -5,21 +5,21 @@ YASMV="./yasmv"
 function test() {
     diff $1-out "$EXAMPLES/$1/$1-out-ref"
     if [ $? == 0 ]; then
-	echo "-- $1 ok"
+	echo "### $1 ok"
     else
     	exit 1
     fi
 }
 
-yasmv --quiet "$EXAMPLES/cannibals/cannibals.smv" < "$EXAMPLES/cannibals/commands" > cannibals-out
+$YASMV --quiet "$EXAMPLES/cannibals/cannibals.smv" < "$EXAMPLES/cannibals/commands" > cannibals-out
 test cannibals
 
-yasmv --quiet "$EXAMPLES/vending/vending.smv" < "$EXAMPLES/vending/commands" > vending-out
+$YASMV --quiet "$EXAMPLES/vending/vending.smv" < "$EXAMPLES/vending/commands" > vending-out
 test vending
 
-yasmv --quiet "$EXAMPLES/herschel/herschel.smv" < "$EXAMPLES/herschel/commands" > herschel-out
+$YASMV --quiet "$EXAMPLES/herschel/herschel.smv" < "$EXAMPLES/herschel/commands" > herschel-out
 test herschel
 
-yasmv --quiet "$EXAMPLES/koenisberg/koenisberg.smv" < "$EXAMPLES/koenisberg/commands" > koenisberg-out
+$YASMV --quiet "$EXAMPLES/koenisberg/koenisberg.smv" < "$EXAMPLES/koenisberg/commands" > koenisberg-out
 test koenisberg
 
