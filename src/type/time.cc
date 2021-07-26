7451e2f8af78fca3851d73881029ecee9d733a80
Markus Wolf <marco.pensallorto@gmail.com>
2021-07-26 19:05:36 +0200

    Added missing files

---
+++ src/type/time.cc
@@ -0,0 +1,32 @@
+/**
+ * @file time.cc
+ * @brief Monolithical type classes implementation
+ *
+ * Copyright (C) 2012 Marco Pensallorto < marco AT pensallorto DOT gmail DOT com >
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ **/
+
+#include <type.hh>
+#include <type_mgr.hh>
+
+TimeType::TimeType(TypeMgr& owner)
+    : Type(owner)
+{ f_repr = f_owner.em().make_time_type(); }
+
+unsigned TimeType::width() const
+{ return 0; }
