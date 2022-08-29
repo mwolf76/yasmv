76b3ec54aad0e57d446c96343e90ee6f74a93cd3
Markus Wolf <marco.pensallorto@gmail.com>
2022-08-29 16:23:37 +0200

    Two minor helper scripts

--- tools/cleanup.sh
+++ tools/cleanup.sh
@@ -2,7 +2,7 @@
 set -e
 
 for x in $(find . -iname "*.cc" -o -iname "*.hh" -o -iname "*.h" -o -iname "*.hh"); do
-    echo -n "$x ..."; 2>/dev/null emacs -batch "$x" -Q -f mark-whole-buffer -f untabify -f whitespace-cleanup -f save-buffer -kill
+    echo -n "$x ..."; 2>/dev/null emacs -batch "$x" -Q -f mark-whole-buffer -f untabify -f delete-trailing-whitespace -f save-buffer -kill
     clang-format -i "$x";
     echo "";
 done
