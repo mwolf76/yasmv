e215ca390226d3b59979ab68b90bc6ab0410fbad
mwolf76 <marco.pensallorto@gmail.com>
2025-07-06 22:38:14 +0200

    Added a ton of short tests

--- tools/run-short-tests.sh
+++ tools/run-short-tests.sh
@@ -59,4 +59,55 @@ test-unfeasible-state expressions/shifts01.smv
 test-unfeasible-state literals/numbers00.smv
 test-unfeasible-state literals/strings00.smv
 
+# arithmetic (extended)
+test-unfeasible-state arithmetic/arithmetic01.smv
+test-unfeasible-state arithmetic/arithmetic02.smv
+test-unfeasible-state arithmetic/arithmetic03.smv
+
+# logical (extended)
+test-unfeasible-state logical/logical03.smv
+test-unfeasible-state logical/logical04.smv
+
+# relational
+test-unfeasible-state relational/relational00.smv
+test-unfeasible-state relational/relational01.smv
+
+# temporal (extended)
+# test-unfeasible-state temporal/temporal03.smv
+# test-unfeasible-state temporal/temporal04.smv
+
+# modules (extended)
+# test-unfeasible-state modules/modules02.smv
+# test-unfeasible-state modules/modules03.smv
+
+# types
+test-unfeasible-state types/types00.smv
+# test-unfeasible-state types/types01.smv
+
+# arrays (extended)
+test-unfeasible-state arrays/array03.smv
+test-unfeasible-state arrays/array04.smv
+
+# nondet (extended)
+# test-unfeasible-state nondet/nondet02.smv
+test-unfeasible-state nondet/nondet03.smv
+
+# words
+test-unfeasible-state words/words00.smv
+# test-unfeasible-state words/words01.smv
+
+# errors (may fail differently)
+test-unfeasible-state errors/errors00.smv || echo " (expected error)"
+# test-unfeasible-state errors/errors01.smv || echo " (expected error)"
+
+# special features
+# test-unfeasible-state special/special00.smv
+# test-unfeasible-state special/special01.smv
+# test-unfeasible-state special/special02.smv
+test-unfeasible-state special/special03.smv
+
+# expressions (extended)
+test-unfeasible-state expressions/xnor00.smv
+test-unfeasible-state expressions/ternary00.smv
+
 echo ""  # one blank line
